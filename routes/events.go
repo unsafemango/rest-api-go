982e3eb3813ab5caec6f54303f4319cad61b8b17
yray akemu <yrayak@outlook.com>
2024-10-29 00:19:29 -0400

    updated api to delete events

--- routes/events.go
+++ routes/events.go
@@ -117,3 +117,36 @@ func updateEvent(context *gin.Context) { // the pointer is so we work on the sin
 		"message": "Event updated successfully.",
 	})
 }
+
+func deleteEvent(context *gin.Context) {
+	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{
+			"message": "Could not parse event id.",
+		})
+		return
+	}
+
+	event, err := models.GetEventByID(eventId)
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{
+			"message": "Could not fetch event.",
+		})
+		return
+	}
+
+	err = event.Delete()
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{
+			"message": "Could not delete the event.",
+		})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{
+		"message": "Event deleted successfully.",
+	})
+}
