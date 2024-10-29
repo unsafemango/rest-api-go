7e1f7a64d8618cfda8e7f209ee702bc85bb21059
yray akemu <yrayak@outlook.com>
2024-10-28 23:36:26 -0400

    updated code to get a single event by id

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
 # REST API
 
 A web server that serves data, recieves data and stores data and can be used as a backend for a mobile app or website.
+
+- **GET** /events
+
+  - `get a list of available events`
+
+- **GET** /events/{id}
+  - `get an event by id`
+- **POST** /events
+  - `create a new bookable event`
+- **PUT** /events/{id}
+  - `update an event`
+- **DELETE** /events/{id}
+  - `delete an event`
+- **POST** /signup
+  - `create a new user`
+- **POST** /login
+  - `authenticate user and create session`
+- **POST** /events/{id}/register
+  - `register user for an event`
+- **DELETE** /events/{id}/register
+  - `cancel event registration`
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 	"unsafemango.com/rest-api-go/db"
@@ -14,12 +15,15 @@ func main() {
 
 	server.GET("/events", getEvents) // handler for incoming http get request
 
+	server.GET("/events/:id", getEvent)
+
 	server.POST("/events", createEvent)
 
 	// starts listening for incoming requests
 	server.Run(":8080") // localhost:8080
 }
 
+// function to get a list of events
 func getEvents(context *gin.Context) {
 	events, err := models.GetAllEvents()
 	if err != nil {
@@ -31,6 +35,29 @@ func getEvents(context *gin.Context) {
 	context.JSON(http.StatusOK, events)
 }
 
+// function to get a single event
+func getEvent(context *gin.Context) {
+	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{
+			"message": "Could not parse event id.",
+		})
+		return
+	}
+
+	events, err := models.GetEventByID(eventId)
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{
+			"message": "Could not fetch event.",
+		})
+		return
+	}
+	context.JSON(http.StatusOK, events)
+}
+
+// function to create an event
 func createEvent(context *gin.Context) {
 	var event models.Event
 	err := context.ShouldBindJSON(&event)
